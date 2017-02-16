0182f34d7f69d3d9a5f997ac3dd45f04c5c77c22
Asa Smith <smith.asa.la@gmail.com>
2017-02-16 10:48:30 -0500

    create updateCart.js

---
+++ public/assets/js/updateCart.js
@@ -0,0 +1,51 @@
+var total = 0
+var addButton = document.getElementById('plus')
+var minusButton = document.getElementById('minus')
+var buttons = document.getElementsByTagName('span')
+
+addButton.addEventListener('click', addCart, false)
+minusButton.addEventListener('click', minusCart, false)
+carousel.addEventListener('click', zeroButton, false)
+
+$(document).click(function(event) {
+    console.log($(event.target).text());
+});
+
+function addCart () {
+  var target = document.getElementById('add-to-cart')
+  var qty = document.getElementById('')
+
+  total += 7
+
+  target.value = 'add ' + total + ' to cart'
+  qty.value = total
+
+  console.log(total)
+}
+
+function minusCart () {
+  var target = document.getElementById('add-to-cart')
+  var qty = document.querySelector('input[name=qty]')
+
+  if (total > 0) {
+    total -= 7
+    target.value = ('add ' + total + ' to cart')
+    qty.value = total
+  } else if (total === 0){
+    target.value = ('add to basket')
+    qty.value = total
+  }
+  console.log(total)
+}
+
+function zeroButton () {
+  var target = document.getElementById('add-to-cart')
+  var qty = document.querySelector('input[name=qty]')
+
+  total = 0
+
+  target.value = 'Add to Cart'
+  qty.value = total
+
+  console.log(total)
+}
