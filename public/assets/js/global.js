64337f2af46ae34783ec0d42c3b69193e2260852
Asa Smith <smith.asa.la@gmail.com>
2017-03-10 09:27:38 -0500

    add global.js

---
+++ public/assets/js/global.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  checkDisplay()
+
+
+// Initialize flickity
+
+  $('.carousel').flickity({
+    cellAlign: 'center',
+    wrapAround: true,
+    isSelected: true,
+    initialIndex: 0
+  })
+
+var $carousel = $('.carousel').flickity()
+
+$carousel.on( 'staticClick.flickity', function( event, pointer, cellElement, cellIndex ) {
+  if ( typeof cellIndex == 'number' ) {
+    $carousel.flickity( 'selectCell', cellIndex );
+  }
+});
+
+// Nav drop down
+
+  $('#nav svg').click(function () {
+    $('nav').fadeToggle('fast')
+    console.log('clicked')
+  })
+
+// Basket dropdown
+
+
+  $('#basket').click(function () {
+    $('#basket-dropdown').slideToggle('slow')
+  })
+})
+
+function checkDisplay () {
+  if ($('p:nth-of-type(2)').css('display') === 'none') {
+    $('#shipping-info').on('click', function () {
+      $('p:nth-of-type(2)').slideToggle('slow')
+    })
+  }
+}
